61a0b694fbf362bbea7e2cc6e8697039a70ac029
Shishatka <Shishatka@gmail.com>
2025-03-18 12:59:53 +0300

    Домашка сделана!

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,6 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        String firstName = "Ivan";
-        String middleName = "Ivanovich";
-        String lastName = "Ivanov";
-        String fullName = "Ivanov Ivan Ivanovich";
-        System.out.println("Ф. И. О. сотрудника - " + fullName);
-
-        fullName = fullName.toUpperCase();
-        System.out.println("Данные Ф.И.О. сотрудника для заполнения отчёта - " + fullName);
-
-        fullName = "Иванов Семён Семёнович";
-        fullName = fullName.replace("ё", "е");
-        System.out.println("Ф. И. О. сотрудника - " + fullName);
-
 
     }
 }
